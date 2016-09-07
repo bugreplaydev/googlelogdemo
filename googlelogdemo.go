bd0dc10083e4598a49ed25fc8d9b28f82128a102
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2016-09-07 01:12:26 +0000

    some cleanup

--- googlelogdemo.go
+++ googlelogdemo.go
@@ -17,11 +17,17 @@ package googlelogdemo
 */
 
 import (
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jwt"
 	logging "google.golang.org/api/logging/v1beta3"
-	"log"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 const (
@@ -44,7 +50,7 @@ type CloudLogger struct {
 	logsID          string
 }
 
-func New(serviceAccount, privateKey, projectID, logsID string) *CloudLogger {
+func New(serviceAccount, privateKey, projectID, logsID string) (*CloudLogger, error) {
 	oauthConf := &jwt.Config{
 		Email:      serviceAccount,
 		PrivateKey: []byte(privateKey),
@@ -54,7 +60,7 @@ func New(serviceAccount, privateKey, projectID, logsID string) *CloudLogger {
 	oauthHTTPClient := oauthConf.Client(oauth2.NoContext)
 	loggingService, err := logging.New(oauthHTTPClient)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	s := logging.NewProjectsService(loggingService)
@@ -63,7 +69,30 @@ func New(serviceAccount, privateKey, projectID, logsID string) *CloudLogger {
 		projectID:       projectID,
 		logsID:          logsID,
 		logEntryService: s.Logs.Entries,
+	}, nil
+}
+
+func NewAppEngineLogger(r *http.Request, projectID, logsID string) (*CloudLogger, error) {
+	ctx, _ := context.WithTimeout(appengine.NewContext(r), time.Second*5)
+	//https://godoc.org/golang.org/x/oauth2/google#example-AppEngineTokenSource
+	loggingService, err := logging.New(&http.Client{
+		Transport: &oauth2.Transport{
+			Source: google.AppEngineTokenSource(ctx, logging.LoggingWriteScope),
+			Base: &urlfetch.Transport{
+				Context: ctx,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
 	}
+	s := logging.NewProjectsService(loggingService)
+	return &CloudLogger{
+		service:         s,
+		projectID:       projectID,
+		logsID:          logsID,
+		logEntryService: s.Logs.Entries,
+	}, nil
 }
 
 func (cl *CloudLogger) WriteLogEntry(severity string, e interface{}) error {
--- googlelogdemo_test.go
+++ googlelogdemo_test.go
@@ -1,8 +1,29 @@
 package googlelogdemo
 
-func ExampleCloudLogger() {
-	logger := New("john@joetown.com", "MYKEY", "digerrity-doo", "logs1")
-	err := logger.WriteLogEntry(LevelDebug, struct {
+import "net/http"
+
+func ExampleCloudLogger_gce() {
+	logger, err := New("myserviceaccount", "myprivatekey", "myprojectid", "mylogname")
+	if err != nil {
+		panic(err)
+	}
+	err = logger.WriteLogEntry(LevelDebug, struct {
+		EventName string
+		EventID   string
+	}{
+		EventName: "NewUser",
+		EventID:   "qxzzr65",
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExampleCloudLogger_appengine() {
+	//normally you'd get the request from a Handler,
+	r := new(http.Request)
+	logger, err := NewAppEngineLogger(r, "myprojectid", "mylogsID")
+	err = logger.WriteLogEntry(LevelDebug, struct {
 		EventName string
 		EventID   string
 	}{
